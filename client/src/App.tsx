2cbffa5a063a66f1d0922e209baf1df01e85d1fd
vladnen97 <darkdescent1991@gmail.com>
2024-07-01 18:02:22 +0300

    feat: add status

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from 'react'
 import { socket } from './socket'
 
 export function App() {
+  const [status, setStatus] = useState<'connected' | 'disconnected'>()
   const [message, setMessage] = useState('')
   const [receivedMessages, setReceivedMessages] = useState<string[]>([])
   const [isAutoScroll, setIsAutoScroll] = useState<boolean>(true)
@@ -15,6 +16,14 @@ export function App() {
 
     socket.on('new message', (msg: string) => {
       setReceivedMessages((state) => [...state, msg])
+    })
+
+    socket.on('connect', () => {
+      setStatus('connected')
+    })
+
+    socket.on('disconnect', () => {
+      setStatus('disconnected')
     })
 
     return () => {
@@ -41,37 +50,63 @@ export function App() {
         flexDirection: 'column',
       }}
     >
-      <h1 style={{ paddingBottom: '40px' }}>Home Page</h1>
-      <div
-        ref={ref}
-        style={{ height: 200, overflowY: 'scroll', width: 300 }}
-        onScroll={(e) => {
-          const maxScrollHeight =
-            e.currentTarget.scrollHeight - e.currentTarget.clientHeight
+      <h1 style={{ paddingBottom: '40px', marginBottom: 0 }}>Home Page</h1>
+      <div>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: '10px',
+            justifyContent: 'left',
+          }}
+        >
+          <h3>Status:</h3>
+          <div
+            style={{
+              width: 14,
+              height: 14,
+              borderRadius: '50%',
+              backgroundColor: status === 'connected' ? 'green' : 'red',
+            }}
+          />
+        </div>
+        <div
+          ref={ref}
+          style={{
+            height: 200,
+            overflowY: 'scroll',
+            width: 300,
+            border: '1px solid black',
+            padding: '10px 0',
+          }}
+          onScroll={(e) => {
+            const maxScrollHeight =
+              e.currentTarget.scrollHeight - e.currentTarget.clientHeight
 
-          if (maxScrollHeight === e.currentTarget.scrollTop) {
-            setIsAutoScroll(true)
-          } else {
-            setIsAutoScroll(false)
-          }
-        }}
-      >
-        {receivedMessages.map((message) => (
-          <div key={message} style={{ padding: '4px 6px' }}>
-            {message}
-          </div>
-        ))}
-      </div>
-      <div style={{ paddingTop: '40px' }}>
-        <input
-          style={{ width: '250px' }}
-          type="text"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <button onClick={sendMessage} disabled={!message.trim()}>
-          send
-        </button>
+            if (maxScrollHeight === e.currentTarget.scrollTop) {
+              setIsAutoScroll(true)
+            } else {
+              setIsAutoScroll(false)
+            }
+          }}
+        >
+          {receivedMessages.map((message) => (
+            <div key={message} style={{ padding: '4px 6px' }}>
+              {message}
+            </div>
+          ))}
+        </div>
+        <div style={{ paddingTop: '40px' }}>
+          <input
+            style={{ width: '250px' }}
+            type="text"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button onClick={sendMessage} disabled={!message.trim()}>
+            send
+          </button>
+        </div>
       </div>
     </main>
   )
